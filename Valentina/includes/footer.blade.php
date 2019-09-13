2d068393751822c67a297237eddb5b7710c22635
Valentina Panetta <valentinaspanetta@gmail.com>
2019-09-13 16:28:36 +0200

    includes, layouts, scss about and references frontend

---
+++ Valentina/includes/footer.blade.php
@@ -0,0 +1,35 @@
+<footer class="footer w-100 bg-white shadow-sm row d-flex justify-content-around text-center">
+	<div class="col-4 p-3">
+		<div >
+			<div>
+				<a href="{{ url('contact') }}" >Contact</a>
+			</div>
+			<div>
+				<a href="{{ url('newsletter') }}" >Newsletter</a>
+			</div>
+		</div>
+	</div>
+	<div class="col-4 p-3">
+		<div >
+			<div>
+				<a href="{{ url('legal') }}" >Legal Details</a>
+			</div>
+			<div>
+				<a href="{{ url('press') }}" >Press</a>
+			</div>
+			<div>
+				<a href="{{ url('socialmedia') }}" >Social Media</a>
+			</div>
+		</div>
+	</div>
+	<div class="col-4 p-3">
+		<div >
+			<div>
+				<a href="{{ url('dataprotection') }}" >Data Protection</a>
+			</div>
+			<div>
+				<a href="{{ url('terms') }}" >Terms and Condition</a>
+			</div>
+		</div>
+	</div>
+</footer>
